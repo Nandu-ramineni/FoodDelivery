86f4fe8e4ce1a2206062a957530ed7927f010497
Nandu-Ramineni <nandurameneni5577@gmail.com>
2024-06-14 17:01:19 +0530

    image api

--- Client/src/Client/Services/api.js
+++ Client/src/Client/Services/api.js
@@ -2,7 +2,8 @@ import axios from "axios";
 
 // const URI = "http://localhost:7000";
 const URI = "https://fooddelivery-xe7w.onrender.com";
-export const URL = "http://localhost:7000"; 
+// export const URL = "http://localhost:7000"; 
+export const URL = "https://fooddelivery-xe7w.onrender.com"; 
 
 export const authenticateSignUp = async(data,token) => {
     try {
