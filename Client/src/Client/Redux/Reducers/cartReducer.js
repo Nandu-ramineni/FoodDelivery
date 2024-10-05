690781c6a467c80fe066b370172a90db0961de00
Ramineni Nandu Varma <123319320+Nandu-ramineni@users.noreply.github.com>
2024-10-05 15:15:47 +0530

    Updated cartReducer.js

--- Client/src/Client/Redux/Reducers/cartReducer.js
+++ Client/src/Client/Redux/Reducers/cartReducer.js
@@ -1,7 +1,7 @@
 import * as actionTypes from "../Constants/cartConstant.js";
 
 const initialState = {
-    cartItems: [],
+    cartItems: JSON.parse(localStorage.getItem('cartItems')) || [],
     currentRestaurant: null,
     error: null
 };
@@ -10,22 +10,24 @@ export const cartReducer = (state = initialState, action) => {
     switch (action.type) {
         case actionTypes.Add_To_Cart_Success:
             const item = action.payload;
-            const restaurantId = action.restaurantId; 
+            const restaurantId = action.restaurantId;
             const existItem = state.cartItems.find(x => x._id === item._id);
+
+            let updatedCartItems;
             if (existItem) {
-                return {
-                    ...state,
-                    cartItems: state.cartItems.map(x => x._id === existItem._id ? item : x),
-                    currentRestaurant: restaurantId 
-                };
+                updatedCartItems = state.cartItems.map(x =>
+                    x._id === existItem._id ? item : x
+                );
             } else {
-                return {
-                    ...state,
-                    cartItems: [...state.cartItems, item],
-                    currentRestaurant: restaurantId 
-                };
+                updatedCartItems = [...state.cartItems, item];
             }
-        
+            localStorage.setItem('cartItems', JSON.stringify(updatedCartItems));
+            return {
+                ...state,
+                cartItems: updatedCartItems,
+                currentRestaurant: restaurantId
+            };
+
         case actionTypes.Add_To_Cart_Failure:
             return {
                 ...state,
@@ -33,15 +35,26 @@ export const cartReducer = (state = initialState, action) => {
             };
 
         case actionTypes.Remove_From_Cart_Success:
+            const filteredCartItems = state.cartItems.filter(x => x._id !== action.payload);
+            localStorage.setItem('cartItems', JSON.stringify(filteredCartItems));
+
             return {
                 ...state,
-                cartItems: state.cartItems.filter(x => x._id !== action.payload)
+                cartItems: filteredCartItems
             };
 
         case actionTypes.Update_Cart_Success:
+            const updatedCart = state.cartItems.map(x =>
+                x._id === action.payload.productId
+                    ? { ...x, quantity: action.payload.quantity }
+                    : x
+            );
+
+            localStorage.setItem('cartItems', JSON.stringify(updatedCart));
+
             return {
                 ...state,
-                cartItems: state.cartItems.map(x => x._id === action.payload.productId ? { ...x, quantity: action.payload.quantity } : x)
+                cartItems: updatedCart
             };
 
         default:
